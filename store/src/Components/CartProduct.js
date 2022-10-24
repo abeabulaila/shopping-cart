5c927f3aba5457d5d31fffa09436b1989ca24786
Ibrahim Abulaila <abeabulaila@gmail.com>
2022-10-24 13:53:44 -0400

    expanded cart functionality with styling. cart functions include adding and deleting, and displaying products within cart

---
+++ store/src/Components/CartProduct.js
@@ -0,0 +1,23 @@
+import { Button } from "react-bootstrap"
+import { CartContext } from "../CartContext"
+import { useContext } from "react"
+import { getProductData } from "../productsStore"
+
+const CartProduct = ({ currentProduct }) => {
+    const cart = useContext(CartContext)
+    const id = currentProduct.id
+    const quantity = currentProduct.quantity
+    const productData = getProductData(id)
+
+    return (
+        <>
+        <h3>{productData.title}</h3>
+        <p>{quantity} total</p>
+        <p>${(quantity * productData.price).toFixed(2)}</p>
+        <Button size='sm' onClick={() => cart.deleteFromCart(id)} >Remove</Button>
+        <hr></hr>
+        </>
+    )
+}
+
+export default CartProduct
--- store/src/Components/Navbar.js
+++ store/src/Components/Navbar.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import { Button, Container, Navbar, Modal } from 'react-bootstrap'
-import { useState } from 'react'
+import { useState, useContext } from 'react'
+import { CartContext } from '../CartContext'
+import CartProduct from './CartProduct'
 
 const NavbarComponent = () => {
+    const cart = useContext(CartContext);
 
     const [show, setShow] = useState(false);
     const handleShow =() => setShow(!show)
-    
+
+    const productsCount = cart.items.reduce((sum, product) => sum + product.quantity, 0)
+
     return (
         <>
             <Navbar expand="sm">
                 <Navbar.Brand href="/">Ecommerce Store</Navbar.Brand>
                 <Navbar.Toggle />
                 <Navbar.Collapse className="justify-content-end">
-                    <Button onClick={handleShow}> Cart 0 Items</Button>
+                    <Button onClick={handleShow}> Cart ({productsCount} Items)</Button>
                 </Navbar.Collapse>
             </Navbar>
             <Modal show={show} onHide={handleShow}>
@@ -21,6 +26,20 @@ const NavbarComponent = () => {
                     <Modal.Title>Shopping Cart</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
+                    {productsCount > 0 ?
+                    <>
+                        <p>Items in your cart:</p>
+                        {cart.items.map((currentProduct, idx) =>(
+                            <CartProduct currentProduct={currentProduct}></CartProduct>
+                        ))}
+
+                        <h1>Total: {cart.getTotalCost().toFixed(2)}</h1>
+
+                        <Button variant="success"> Purchase Items!</Button>
+                    </>
+                    :
+                    <h1>There are no items in your cart!</h1>    
+                }
                     <h1>This is the modal body.</h1>
                 </Modal.Body>
             </Modal>
